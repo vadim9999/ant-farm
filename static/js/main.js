83db280341b5c2d0c3abfa952c371973cd70ed82
Vadim <vadim.putrov@gmail.com>
2019-03-27 13:53:56 +0200

    add red circle

--- static/js/main.js
+++ static/js/main.js
@@ -30,8 +30,55 @@ window.setInterval(function(){
 // console.log('Code ' + event.code + ' reason '+ event.reason)
 // }
 // *********************************************
+var h1 = document.getElementById('stopWatch'),
+    start = document.getElementById('start'),
+    stop = document.getElementById('stop'),
+    clear = document.getElementById('clear'),
+    seconds = 0, minutes = 0, hours = 0,
+    t;
 
-function loadDoc(){
+function add() {
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
+    
+    h1.innerHTML = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+
+    timer();
+}
+function timer() {
+    document.getElementById("blinkingCircle").style.backgroundColor = "red";
+    document.getElementById("blinkingCircle").style.animation = "blinker 1.5s cubic-bezier(.5, 0, 1, 1) infinite alternate"
+    t = setTimeout(add, 1000);
+}
+// timer();
+
+
+/* Start button */
+start.onclick = timer;
+
+/* Stop button */
+stop.onclick = function() {
+    clearTimeout(t);
+    document.getElementById("blinkingCircle").style.backgroundColor = "";
+    document.getElementById("blinkingCircle").style.animation = ""
+}
+
+/* Clear button */
+clear.onclick = function() {
+    h1.innerHTML = "00:00:00";
+    seconds = 0; minutes = 0; hours = 0;
+}
+
+
+
+   function loadDoc(){
 
   var xhttp = new XMLHttpRequest();
 
