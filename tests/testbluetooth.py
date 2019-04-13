7c55ab717b7ee86cfc0fdca9cb718e727c4d2586
Vadim <vadim.putrov@gmail.com>
2019-04-13 23:52:49 +0300

    add getting data from android

--- tests/testbluetooth.py
+++ tests/testbluetooth.py
@@ -22,18 +22,37 @@ print("Waiting for connection on RFCOMM channel %d" % port)
 
 client_sock, client_info = server_sock.accept()
 print("Accepted connection from ", client_info)
-
+router = "router"
+password = "password"
 try:
     while True:
+
         data = client_sock.recv(1024)
-        if len(data) == 0: break
+        # if len(data) == 0: break
         print("received [%s]" % data)
-        client_sock.send("Received data!\n".encode("utf-8"))
+        print(data.decode("utf-8"))
+        decodedData = str(data.decode("utf-8"))
+        receivedData = decodedData.split("_")
+        print(receivedData)
+        print(decodedData == "getWIFIData")
+
+        if receivedData[0] == "getWIFIData":
+            print("write response wifi")
+            client_sock.send(("{\"name\": \"getWIFIData\", \"router\":\"" + router + "\",\"password\":\"" +  password +"\"}\n").encode("utf-8"))
+        if receivedData[0] == "setWIFIData":
+            router = receivedData[1]
+            password = receivedData[2]
+            print(router) 
+            print(password)
+        # if decodedData == "setWIFIData\n"
+        # client_sock.send("Received data!\n".encode("utf-8"))
 except IOError:
     pass
+finally:
+    print("disconnected")
 
-print("disconnected")
+    client_sock.close()
+    server_sock.close()
+    print("all done")
 
-client_sock.close()
-server_sock.close()
-print("all done")
+
