268f61f3bd16767bf588482203cc220ce86b7d66
Vadim <vadim.putrov@gmail.com>
2019-05-22 21:59:35 +0300

    clean

--- tests/cookietest.py
+++
@@ -1,8 +0,0 @@
-from http import cookies
-
-C = cookies.SimpleCookie()
-# C["rocky"] = "road"
-# C["rocky"]["path"] = "/cookie"
-# print(C.output(attrs = [], header = "Cookie:"))
-C.load("chips=ahoy; vienna=finger")
-print(C)
--- tests/formattest.py
+++
@@ -1,3 +0,0 @@
-temperature = 25
-humidity = 60
-print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
