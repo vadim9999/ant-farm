619f5b89ff6dad215760468136d4daad06eef27e
Vadim <vadim.putrov@gmail.com>
2019-05-26 15:02:37 +0300

    clean sendResolution

--- static/js/sendResolution.js
+++ static/js/sendResolution.js
@@ -3,114 +3,87 @@ var resolution = "640x480";
 
 
 function onQ720() {
-    
-    console.log("in function___720______");
-    resolution = "1024x768"
-    document.getElementById("Q720").setAttribute("class", item + " active")
-    document.getElementById("Q480").setAttribute("class", item)
-    document.getElementById("Q240").setAttribute("class", item)
-    onChangeQuality()
-    // document.getElementById("Q240").setAttribute("disabled", "true")
-    // document.getElementById('info').removeAttribute("title")
-    // $('#videoResolution').prop('disabled', true);
-    // document.getElementById("info").removeAttribute("style")
-    // $('#info').tooltip('enable')
+  resolution = "1024x768"
+  document.getElementById("Q720").setAttribute("class", item + " active")
+  document.getElementById("Q480").setAttribute("class", item)
+  document.getElementById("Q240").setAttribute("class", item)
+  onChangeQuality()
 }
 
-
-
 function onQ480() {
-
-    resolution = "640x480"
-    document.getElementById("Q720").setAttribute("class", item)
-    document.getElementById("Q480").setAttribute("class", item + " active")
-    document.getElementById("Q240").setAttribute("class", item)
-    onChangeQuality()
+  resolution = "640x480"
+  document.getElementById("Q720").setAttribute("class", item)
+  document.getElementById("Q480").setAttribute("class", item + " active")
+  document.getElementById("Q240").setAttribute("class", item)
+  onChangeQuality()
 }
 
 function onQ240() {
-    resolution = "320x240"
-    document.getElementById("Q720").setAttribute("class", item)
-    document.getElementById("Q480").setAttribute("class", item)
-    document.getElementById("Q240").setAttribute("class", item + " active")
-    onChangeQuality()
+  resolution = "320x240"
+  document.getElementById("Q720").setAttribute("class", item)
+  document.getElementById("Q480").setAttribute("class", item)
+  document.getElementById("Q240").setAttribute("class", item + " active")
+  onChangeQuality()
 }
 
-function onChangeQuality(){
-  if (isPreviewStart){
+function onChangeQuality() {
+  if (isPreviewStart) {
     stopPreview()
     setTimeout(function () {
       startPreview()
     }
       , 1000);
-    
+
   }
 }
 
 function startPreview() {
-    userId = getUrlParam('id', 'Empty')
-    isPreviewStart = true
-    
-    enableButtonStop('preview')
-    document.getElementById("start-record").removeAttribute("disabled")
-    document.getElementById("capture-image").removeAttribute("disabled")
-  
-    var xhttp = new XMLHttpRequest();
-    streamUrl = location.protocol + "//" + location.host + "/stream.mjpg?id=" + userId;
-    console.log(streamUrl);
-  
-    xhttp.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-  
-        console.log("POST /start _____");
-        console.log(this.responseText);
-        document.getElementById('badge').src = streamUrl;
-      }
-    }
-    xhttp.open("POST", "/start?id=" + userId, true);
-    xhttp.send(resolution);
-  
-  }
+  userId = getUrlParam('id', 'Empty')
+  isPreviewStart = true
 
-  function stopPreview() {
-    userId = getUrlParam('id', 'Empty')
-    enableButtonStart("preview")
-    isPreviewStart = false
-    isBlocked = false
-    document.getElementById("capture-image").disabled = true;
-    document.getElementById("start-record").disabled = true;
-    var xhttp = new XMLHttpRequest();
-  
-    xhttp.onreadystatechange = function () {
-      if (this.readyState == 4) {
-        console.log("GET");
-        console.log(this.responseText);
-      }
-    }
-    xhttp.open("GET", "/stop?id=" + userId, true);
-    xhttp.send();
-  }
+  enableButtonStop('preview')
+  document.getElementById("start-record").removeAttribute("disabled")
+  document.getElementById("capture-image").removeAttribute("disabled")
 
-  // ----------stream-------------
+  var xhttp = new XMLHttpRequest();
+  streamUrl = location.protocol + "//" + location.host + "/stream.mjpg?id=" + userId;
+
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById('badge').src = streamUrl;
+    }
+  }
+  xhttp.open("POST", "/start?id=" + userId, true);
+  xhttp.send(resolution);
+}
+
+function stopPreview() {
+  userId = getUrlParam('id', 'Empty')
+  enableButtonStart("preview")
+  isPreviewStart = false
+  isBlocked = false
+  document.getElementById("capture-image").disabled = true;
+  document.getElementById("start-record").disabled = true;
+  var xhttp = new XMLHttpRequest();
+
+  xhttp.open("GET", "/stop?id=" + userId, true);
+  xhttp.send();
+}
+
+// ----------stream-------------
 function startStream() {
   userId = getUrlParam('id', 'Empty')
   startBlinking("blinkingStream")
-  // document.getElementById("stop-" + id).removeAttribute("disabled")
-  // document.getElementById("start-" + id).disabled = "true"
-  
+
   enableButtonStop("stream")
   var e = document.getElementById("resolutionStream")
   var resolution = e.options[e.selectedIndex].value;
-
-  console.log("resolution");
 
   var xhttp = new XMLHttpRequest();
 
   xhttp.onreadystatechange
     = function () {
       if (this.readyState == 4) {
-        console.log("POST /start_stream");
-        console.log(this.responseText);
       }
     }
   xhttp.open("POST", "/start_stream?id=" + userId, true);
@@ -125,14 +98,6 @@ function stopStream() {
   enableButtonStart("preview")
   isPreviewStart = false
   var xhttp = new XMLHttpRequest();
-
-  xhttp.onreadystatechange
-    = function () {
-      if (this.readyState == 4) {
-        console.log("GET /stop_stream");
-        console.log(this.responseText);
-      }
-    }
   xhttp.open("GET", "/stop_stream?id=" + userId, true);
   xhttp.send();
 
@@ -145,11 +110,9 @@ function waitStartPreview() {
   xhttp.onreadystatechange
     = function () {
       if (this.readyState == 4) {
-        console.log("GET /wait_start_preview");
-        console.log(this.responseText);
         startPreview()
         document.getElementById("capture-image").disabled = true;
-    document.getElementById("start-record").disabled = true;
+        document.getElementById("start-record").disabled = true;
       }
     }
   xhttp.open("GET", "/wait_start_preview?id=" + userId, true);
