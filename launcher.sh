13fd5221a32dc27210ef2fc62106b3608b9a2fb5
Vadim <vadim.putrov@gmail.com>
2019-06-06 00:07:03 +0300

    delete sh

--- launcher.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# launcher.sh
-# navigate to home directory, then to this directory, then execute python script, then back home
-
-cd /
-cd /home/pi/ant-farm
-sudo python3.7 setup.py
-cd /
--- launcherServoblaster.sh
+++
@@ -1,2 +0,0 @@
-cd /
-sudo /usr/local/sbin/servod --idle-timeout=2000 --p1pins=12
